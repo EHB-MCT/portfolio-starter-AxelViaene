a8c1d567f88d5dd4fa064463160b5e81f95a9dc1
AxelViaene <56039850+AxelViaene@users.noreply.github.com>
2023-10-12 10:31:22 +0200

    feat(database): added local database
    Added a local database with use of node module 'sqlite3'

--- Les1/src/index.js
+++ Les1/src/index.js
@@ -1,9 +1,13 @@
-console.log('Hello world')
 const express = require('express')
 const sqlite3 = require('sqlite3').verbose();
+
 const app = express()
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
+
+const db = require('../db/database.js')
+
 app.listen(port, (err) => {
     if(!err) {
         console.log("running on port" + port)
