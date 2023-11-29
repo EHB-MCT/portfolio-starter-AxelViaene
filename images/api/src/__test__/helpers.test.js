02bf4c2ec1f0f47a013b7aae5a64472cf85363e5
Axel Viaene <axel.viaene@student.ehb.be>
2023-11-29 17:00:13 +0100

    feat: First unit test
    Test to check if username is formatted correctly

---
+++ images/api/src/__test__/helpers.test.js
@@ -0,0 +1,13 @@
+const {checkUserName} = require("./../helpers/endpointHelpers.js")
+
+test("check name", () => {
+
+  expect(checkUserName("")).toBe(false)
+  expect(checkUserName(null)).toBe(false)
+  expect(checkUserName("i")).toBe(false)
+  expect(checkUserName(2)).toBe(false)
+  expect(checkUserName("lefhslhfnlskflskflksflksfsbflsebfmsfmsf")).toBe(false)
+
+  expect(checkUserName("Jan")).toBe(true)
+  expect(checkUserName("Anne Sophie")).toBe(true)
+})
